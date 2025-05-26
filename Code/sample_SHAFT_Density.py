c5efab49acd46fe37bf58bdc618d46b0e81b64d1
Nguyen Minh Tri <nguyenminhtri0394@gmail.com>
2025-05-26 10:16:07 +1000

    Update sample_SHAFT_Density.py

--- Code/sample_SHAFT_Density.py
+++ Code/sample_SHAFT_Density.py
@@ -18,12 +18,9 @@ def run(cfg: DictConfig) -> None:
     if cfg.train.deterministic:
         seed_everything(cfg.train.random_seed)
 
-    print('Instantiate CHGFlownet')
-    chgflownet = hydra.utils.instantiate(cfg.chgflownet.chgflownet)
-    # chgflownet.sample_with_threshold(number_to_sample=1000, save_folder='/sampled_maxblock6_bscorethreshold_054_reward_threshold_06_max_300_seed_1_big_lattice/', bscorethreshold=0.6, reward_threshold=0.7)
-    # chgflownet.sample_with_threshold(number_to_sample=1000, save_folder='/sampled_maxblock4_bscorethreshold_065_reward_threshold_07_max_200_seed_34/', reward_threshold=0.7)
-    # chgflownet.sample(10000)
-    chgflownet.sample_with_threshold_and_type(number_to_sample=1000, save_folder='/sample_ternary_db_s18/', bscorethreshold=0.55, reward_threshold=0.7, type='ternary')
+    print('Instantiate SHAFT')
+    shaft = hydra.utils.instantiate(cfg.SHAFT.SHAFT_Density)
+    shaft.sample_with_threshold_and_type(number_to_sample=1000, save_folder='/sample_ternary_db/', bscorethreshold=0.55, reward_threshold=0.7, type='ternary')
 
 @hydra.main(config_path=str(PROJECT_ROOT / "config"), config_name="sample")
 def main(cfg: omegaconf.DictConfig):
